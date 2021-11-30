395742102b5397f0c3e0828c7164518eeb55dc5a
Craig Neff <ifly12610@gmail.com>
2021-11-30 00:33:24 -0700

    workong on Navbar comp

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SiAirbnb } from "react-icons/si";
+import { FaBeer } from "react-icons/fa";
+
+export default function Navbar() {
+  return (
+    <div className="Navbar">
+      <h1 className="Navbar_title">
+        <SiAirbnb />
+        airbnb
+      </h1>
+    </div>
+  );
+}
---
+++ src/components/Test.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FaBeer } from "react-icons/fa";
+
+export default function Test() {
+  return (
+    <div>
+      <h1 className="Test">
+        This is Working!
+        <FaBeer />
+      </h1>
+    </div>
+  );
+}
