f3d6b116d769bc3eff74ead858117fe77a75e8f2
Craig Neff <ifly12610@gmail.com>
2021-12-02 23:21:57 -0700

    working on dynamic componenets section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
-      <img src="../images/katie-zaferes.png" className="card_img" />
+      <img src={`../images/${props.img}`} className="card_img" />
       <div className="card_stats">
         <img src="../images/star.png" alt="star" className="card_star" />
-        <span>5.0</span>
-        <span className="gray">(6)•</span>
-        <span className="gray">USA </span>
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount})•</span>
+        <span className="gray">{props.country} </span>
       </div>
-      <p>Life Lessons with Katie Zaferes</p>
+      <p className="title">{props.title}</p>
       <p>
-        <span className="bold">From $136</span> / person
+        <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
   );
